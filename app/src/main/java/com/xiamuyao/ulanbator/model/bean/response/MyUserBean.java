4f0178b858d7e8a66bb73e836e4a5c24fc709233
沐尧 <xiamuyaodqx@gmail.com>
2019-07-31 17:57:12 +0800

    [add] 我的部分接口定义

---
+++ app/src/main/java/com/xiamuyao/ulanbator/model/bean/response/MyUserBean.java
@@ -0,0 +1,121 @@
+package com.xiamuyao.ulanbator.model.bean.response;
+
+public class MyUserBean {
+
+    /**
+     * result : {"returnCode":"0","returnUserMessage":"成功","returnMessage":"成功"}
+     * data : {"nickname":"MAX","vipType":0,"tel":"15000000001","dialingCode":"86","inviteCode":"53391699"}
+     */
+
+    private ResultBean result;
+    private DataBean data;
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public static class ResultBean {
+        /**
+         * returnCode : 0
+         * returnUserMessage : 成功
+         * returnMessage : 成功
+         */
+
+        private String returnCode;
+        private String returnUserMessage;
+        private String returnMessage;
+
+        public String getReturnCode() {
+            return returnCode;
+        }
+
+        public void setReturnCode(String returnCode) {
+            this.returnCode = returnCode;
+        }
+
+        public String getReturnUserMessage() {
+            return returnUserMessage;
+        }
+
+        public void setReturnUserMessage(String returnUserMessage) {
+            this.returnUserMessage = returnUserMessage;
+        }
+
+        public String getReturnMessage() {
+            return returnMessage;
+        }
+
+        public void setReturnMessage(String returnMessage) {
+            this.returnMessage = returnMessage;
+        }
+    }
+
+    public static class DataBean {
+        /**
+         * nickname : MAX
+         * vipType : 0
+         * tel : 15000000001
+         * dialingCode : 86
+         * inviteCode : 53391699
+         */
+
+        private String nickname;
+        private int vipType;
+        private String tel;
+        private String dialingCode;
+        private String inviteCode;
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+
+        public int getVipType() {
+            return vipType;
+        }
+
+        public void setVipType(int vipType) {
+            this.vipType = vipType;
+        }
+
+        public String getTel() {
+            return tel;
+        }
+
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
+
+        public String getDialingCode() {
+            return dialingCode;
+        }
+
+        public void setDialingCode(String dialingCode) {
+            this.dialingCode = dialingCode;
+        }
+
+        public String getInviteCode() {
+            return inviteCode;
+        }
+
+        public void setInviteCode(String inviteCode) {
+            this.inviteCode = inviteCode;
+        }
+    }
+}
+
---
+++ app/src/main/java/com/xiamuyao/ulanbator/model/bean/response/SetUserBean.java
@@ -0,0 +1,64 @@
+package com.xiamuyao.ulanbator.model.bean.response;
+
+public class SetUserBean {
+
+    /**
+     * result : {"returnCode":"0","returnUserMessage":"成功","returnMessage":"成功"}
+     * data : null
+     */
+
+    private ResultBean result;
+    private Object data;
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public static class ResultBean {
+        /**
+         * returnCode : 0
+         * returnUserMessage : 成功
+         * returnMessage : 成功
+         */
+
+        private String returnCode;
+        private String returnUserMessage;
+        private String returnMessage;
+
+        public String getReturnCode() {
+            return returnCode;
+        }
+
+        public void setReturnCode(String returnCode) {
+            this.returnCode = returnCode;
+        }
+
+        public String getReturnUserMessage() {
+            return returnUserMessage;
+        }
+
+        public void setReturnUserMessage(String returnUserMessage) {
+            this.returnUserMessage = returnUserMessage;
+        }
+
+        public String getReturnMessage() {
+            return returnMessage;
+        }
+
+        public void setReturnMessage(String returnMessage) {
+            this.returnMessage = returnMessage;
+        }
+    }
+}
